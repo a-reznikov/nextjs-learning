fd38e414d73c4d51aebe0571aeb3621e1b96b863
a-reznikov <a.reznnikov@gmail.com>
2024-01-30 14:51:32 +0300

    feat: add header and footer

---
+++ src/components/common/header/header.tsx
@@ -0,0 +1,11 @@
+const Header: React.FC = () => {
+  return (
+    <header className="bg-stone-900">
+      <h2 className="text-slate-100 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+        NextJS
+      </h2>
+    </header>
+  );
+};
+
+export { Header };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { Footer } from "@/components/common/footer/footer";
+import { Header } from "@/components/common/header/header";
+
 export default function Home() {
-  return <h1>Initial project</h1>;
+  return (
+    <div className="app">
+      <Header />
+      <Footer />
+    </div>
+  );
 }

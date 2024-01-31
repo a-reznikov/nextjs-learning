4eda3b4be2f557488021994bd45ef356430ebb24
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-01-31 09:25:57 +0300

    feat: add the logo and styles to the header according to the design

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 const Header: React.FC = () => {
   return (
-    <header className="bg-stone-900 p-3">
-      <h2 className="text-slate-100 text-2xl font-bold">NextJS</h2>
+    <header className="w-full shadow-[0px_2px_10px_rgba(22,22,22,0.1)]">
+      <div className="flex justify-between items-center max-w-[1440px] px-[120px] mx-auto">
+        <Image
+          className="my-4"
+          src="/logo.svg"
+          width={129}
+          height={28}
+          alt="Logo"
+        />
+        <nav>Navigation</nav>
+      </div>
     </header>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
-import { Header } from "@/components/common/header/Header";
-import { Footer } from "@/components/common/footer/Footer";
+import { MainPage } from "@/components/mainPage/MainPage";
 
 export default function Home() {
-  return (
-    <div className="home flex flex-col justify-between h-screen">
-      <Header />
-      <Footer />
-    </div>
-  );
+  return <MainPage />;
 }

552d39157e9f4e616ebd27934d2599ddbd9e6205
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-02 16:54:04 +0300

    feat: add pages for details and sections

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { RootLayout } from "@/layouts/root-layout/RootLayout";
 import { OPEN_SANS } from "@/fonts";
 import "@/styles/globals.css";
 
@@ -9,7 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <main className={OPEN_SANS.className}>
-        <Component {...pageProps} />
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
       </main>
     </QueryClientProvider>
   );

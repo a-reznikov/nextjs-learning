7785b1517d0089ead98a1ce583e1d642ea0f834a
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-02 15:38:39 +0300

    feat: migrate to classNames

--- src/components/common/news-card/NewsCard.tsx
+++ src/components/common/news-card/NewsCard.tsx
@@ -1,16 +1,32 @@
 import Image from "next/image";
+import classNames from "classnames";
 import { News } from "../../../api/news-section/types";
 
 export const NewsCard: React.FC<{ news: News }> = ({ news }) => {
   const { section, date, title, abstract, imgUrl, imgAlt } = news;
 
   return (
-    <article className="flex flex-col lg:flex-row shadow-section justify-between">
-      <div className="flex flex-wrap lg:flex-col lg:justify-normal justify-between items-center lg:items-start py-5 px-3.75 lg:p-10 gap-5 text-dark">
+    <article
+      className={classNames(
+        "flex flex-col shadow-section justify-between",
+        "lg:flex-row"
+      )}
+    >
+      <div
+        className={classNames(
+          "flex flex-wrap justify-between items-center py-5 px-3.75 gap-5 text-dark",
+          "lg:flex-col lg:justify-normal lg:items-start lg:p-10"
+        )}
+      >
         <div className="py-1 px-2 bg-main rounded first-letter:capitalize text-light text-base leading-subtext font-semibold">
           {section}
         </div>
-        <p className="text-sm leading-subtext lg:order-last lg:flex lg:grow lg:items-end">
+        <p
+          className={classNames(
+            "text-sm leading-subtext",
+            "lg:order-last lg:flex lg:grow lg:items-end"
+          )}
+        >
           {date}
         </p>
         <div className="flex flex-col gap-4">
@@ -20,7 +36,10 @@ export const NewsCard: React.FC<{ news: News }> = ({ news }) => {
       </div>
 
       <Image
-        className="w-full basis-full object-cover min-h-70 max-h-70 lg:w-0 lg:basis-51 lg:shrink-0 lg:min-h-114.5 lg:max-h-114.5"
+        className={classNames(
+          "w-full basis-full object-cover min-h-70 max-h-70",
+          "lg:w-0 lg:basis-51 lg:shrink-0 lg:min-h-114.5 lg:max-h-114.5"
+        )}
         src={imgUrl}
         width={375}
         height={280}

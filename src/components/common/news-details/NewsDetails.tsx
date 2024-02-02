d8504722c05b6c65e8030a266406c08368c91af6
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-02 18:11:42 +0300

    feat: use react query as state manager

--- src/components/common/news-details/NewsDetails.tsx
+++ src/components/common/news-details/NewsDetails.tsx
@@ -1,3 +1,15 @@
+import { getNewsBySection } from "@/api/news-section/module";
+import { HEADER_NAVIGATION } from "@/constants/links";
+import { useQuery } from "@tanstack/react-query";
+
 export const NewsDetails: React.FC = () => {
+  const sectionName = HEADER_NAVIGATION[0].href.slice(1);
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["section", sectionName],
+    queryFn: () => getNewsBySection(sectionName),
+  });
+
+  //TODO get details from React Query state
+
   return <div>News Details</div>;
 };

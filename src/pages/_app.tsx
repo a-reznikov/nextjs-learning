e2c35263b5e4b38b2aff61923eee61c21dd90247
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-05 12:14:26 +0300

    refactor: create constant for query client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { QUERY_CLIENT } from "@/constants/query-client";
 import { OPEN_SANS } from "@/fonts";
 import "@/styles/globals.css";
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={QUERY_CLIENT}>
       <main className={OPEN_SANS.className}>
         <Component {...pageProps} />
       </main>

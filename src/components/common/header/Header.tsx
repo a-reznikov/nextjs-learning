4a7137f76316ca112fefee960fa64f6ea1a6f04f
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-06 14:11:51 +0300

    fix: delete the unnecessary file

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 import { HEADER_NAVIGATION } from "@/constants/links";
 import Logo from "/public/icons/logo.svg";
 import Menu from "/public/icons/menu.svg";
-import { MenuSvg } from "./MenuSvg";
 
 export const Header: React.FC = () => {
   const router = useRouter();
@@ -60,7 +59,6 @@ export const Header: React.FC = () => {
           )}
         >
           <Menu />
-          <MenuSvg />
         </a>
       </div>
     </header>

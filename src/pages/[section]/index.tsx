607b31636c7299be5ed67dc685d3f10b238c9134
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-08 20:06:03 +0300

    feat: implement ssr with getServerSideProps for SectionPage

--- src/pages/[section]/index.tsx
+++ src/pages/[section]/index.tsx
@@ -1,15 +1,35 @@
-import { useRouter } from "next/router";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
+import { dehydrate } from "@tanstack/react-query";
 import { NewsList } from "@/components/news-list/NewsList";
 import { hasSlugStringType } from "@/utils/type-guards";
+import { prefetchNews } from "@/api/news/queries";
+import { QUERY_CLIENT } from "@/constants/query-client";
 
-const SectionPage: NextPage = () => {
-  const router = useRouter();
-  const sectionName = hasSlugStringType(router.query.section)
-    ? router.query.section
+type Props = {
+  section: string;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const section = hasSlugStringType(context.query.section)
+    ? context.query.section
     : "";
 
-  return <NewsList section={sectionName} />;
+  console.log(section);
+
+  await prefetchNews(section);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(QUERY_CLIENT),
+      section,
+    },
+  };
+};
+
+const SectionPage: NextPage<Props> = ({ section }) => {
+  return <NewsList section={section} />;
 };
 
 export default SectionPage;

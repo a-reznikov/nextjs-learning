5e3796abe52a37f533a03acec6544a76d184d8c3
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-08 14:25:51 +0300

    feat: implement throw Error after fetching

--- src/api/articles/module.ts
+++ src/api/articles/module.ts
@@ -3,15 +3,18 @@ import {
   API_SERVICE_ARTICLES,
   API_KEY,
 } from "@/constants/variables";
-import { Article, ArticlesResponse } from "./types";
+import { ArticlesResponse } from "./types";
 import { prepareArticle } from "./formatter";
 
-export const searchArticles = async (
-  query: string
-): Promise<Article | null> => {
+export const searchArticles = async (query: string) => {
   const result: Response = await fetch(
     `${API_BASE_URL}${API_SERVICE_ARTICLES}articlesearch.json?q=${query}&api-key=${API_KEY}`
   );
+
+  if (!result.ok) {
+    throw new Error("Oops. Something bad happened!");
+  }
+
   const { response }: ArticlesResponse = await result.json();
 
   return prepareArticle(response.docs);
--- src/api/news/module.ts
+++ src/api/news/module.ts
@@ -2,14 +2,16 @@ import { API_BASE_URL, API_SERVICE_NEWS, API_KEY } from "@/constants/variables";
 import { NycTimesResponse, News } from "@/api/news/types";
 import { prepareData } from "./formatter";
 
-export const getNewsBySection = async (
-  sectionName: string
-): Promise<News[]> => {
+export const getNewsBySection = async (sectionName: string) => {
   const response: Response = await fetch(
     `${API_BASE_URL}${API_SERVICE_NEWS}${sectionName}.json?api-key=${API_KEY}`
   );
+
+  if (!response.ok) {
+    throw new Error("Oops. Something bad happened!");
+  }
+
   const { results }: NycTimesResponse = await response.json();
 
-  
-return prepareData(results);
+  return prepareData(results);
 };

5f3cebacf1aba86de133b6ec72508f44dcf6af30
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-13 19:22:12 +0300

    feat: install heroicons and add Pagination component to project

---
+++ src/components/common/pagination/Pagination.tsx
@@ -0,0 +1,71 @@
+import classNames from "classnames";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+
+export const Pagination: React.FC = () => {
+  return (
+    <div
+      className={classNames(
+        "flex items-center justify-between bg-white px-4 py-3",
+        "sm:px-6"
+      )}
+    >
+      <div className={classNames("flex flex-1 justify-between", "sm:hidden")}>
+        <a
+          href="#"
+          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Previous
+        </a>
+        <a
+          href="#"
+          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Next
+        </a>
+      </div>
+      <div
+        className={classNames(
+          "hidden",
+          "sm:flex sm:flex-1 sm:items-center sm:justify-between"
+        )}
+      >
+        <div>
+          <p className="text-sm text-gray-700 font-medium">
+            Showing 1 to 3 of 3 stories
+          </p>
+        </div>
+        <div>
+          <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm">
+            <a
+              href="#"
+              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            >
+              <ChevronLeftIcon className="h-5 w-5" />
+            </a>
+            <a
+              href="#"
+              className="relative z-10 inline-flex items-center bg-main px-4 py-2 text-sm font-semibold text-white"
+            >
+              1
+            </a>
+            <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300">
+              ...
+            </span>
+            <a
+              href="#"
+              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            >
+              1
+            </a>
+            <a
+              href="#"
+              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            >
+              <ChevronRightIcon className="h-5 w-5" />
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/dream-stories/StoriesList.tsx
+++ src/components/dream-stories/StoriesList.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import classNames from "classnames";
 import { useStories } from "@/api/stories/queries";
 import { Loader } from "../common/loader/Loader";
+import { Pagination } from "../common/pagination/Pagination";
 import { StoryCard } from "./components/story-card/StoryCard";
 
 export const StoriesList: React.FC = () => {
@@ -16,20 +17,23 @@ export const StoriesList: React.FC = () => {
   }
 
   return (
-    <ul className="flex flex-col gap-5">
-      {data?.map((story, index) => (
-        <li
-          key={story.id}
-          className={classNames(
-            "transition duration-300 ease-out hover:ease-in",
-            "md:hover:scale-105"
-          )}
-        >
-          <Link href={`dream-stories/${story.id}`}>
-            <StoryCard story={story} index={index} />
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <section className="flex flex-col gap-y-8">
+      <ul className="flex flex-col gap-5">
+        {data?.map((story, index) => (
+          <li
+            key={story.id}
+            className={classNames(
+              "transition duration-300 ease-out hover:ease-in",
+              "md:hover:scale-105"
+            )}
+          >
+            <Link href={`dream-stories/${story.id}`}>
+              <StoryCard story={story} index={index} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Pagination />
+    </section>
   );
 };
